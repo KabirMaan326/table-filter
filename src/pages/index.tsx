a52b997d21f338e39260a3a7489759fda9ad652b
Kabir Maan <kabirmaan326@gmail.com>
2023-01-24 16:38:24 +0000

    feature: create basic home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
-export default function Index() {
-  return <></>;
-}
+import {
+	GetServerSideProps,
+	InferGetServerSidePropsType,
+	type NextPage,
+} from "next";
+
+const Home: NextPage = ({
+	rainfallData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+	return (
+		<div>
+			<h1>Home</h1>
+		</div>
+	);
+};
+
+// This gets called on every request
+export const getServerSideProps: GetServerSideProps = async () => {
+	return { props: { rainfallData: [] } };
+};
+
+export default Home;

1b77d68c21c273e90a47fda1f62d927a99c7504e
Kabir Maan <kabirmaan326@gmail.com>
2023-01-24 16:43:13 +0000

    feature: get rainfallData from getServerSideProps and transform

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,22 @@ import {
 	InferGetServerSidePropsType,
 	type NextPage,
 } from "next";
+import { useMemo } from "react";
+import { rainfallData } from "../utils/data";
+import { getTableDataFromRainfall } from "../utils/dataTransformation";
 
 const Home: NextPage = ({
 	rainfallData,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+	// We memoize this value so it only calculates if the rainfallData changes
+	// In this case as data is returned on each request by getServerSideProps it
+	// is not necessary to use this but if we were to request the data again
+	// and set it in state then its useful to memoize the return value
+	const { tableData, columns } = useMemo(
+		() => getTableDataFromRainfall(rainfallData),
+		[rainfallData]
+	);
+	console.log({ tableData, columns });
 	return (
 		<div>
 			<h1>Home</h1>
@@ -16,7 +28,7 @@ const Home: NextPage = ({
 
 // This gets called on every request
 export const getServerSideProps: GetServerSideProps = async () => {
-	return { props: { rainfallData: [] } };
+	return { props: { rainfallData } };
 };
 
 export default Home;
